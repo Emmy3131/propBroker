f7ed42e3b392970871fa2e3653906e8be8056a0c
Emmy3131 <devinechild3003@gmail.com>
2026-09-16 22:14:48 +0100

    wip

--- index.js
+++ index.js
@@ -5,8 +5,39 @@ const helmet = require("helmet");
 const app = express();
 const rateLimit = require("express-rate-limit");
 const hpp = require("hpp");
+const cors = require("cors");
 
 app.use(helmet());
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://prop-broker.vercel.app/"
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin
+      // such as Postman/server-to-server requests
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+
+    credentials: true,
+
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+
+    allowedHeaders: ["Content-Type", "Authorization", "X-CSRF-Token"],
+  }),
+);
+
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000,
   max: 100,
@@ -20,20 +51,10 @@ const limiter = rateLimit({
   },
 });
 
-
 app.use("/api/v1", limiter);
 app.use(hpp());
 app.use(express.json());
 app.set("query parser", "extended");
-
-
-
-// app.get("/", (req, res) => {
-//   res.status(200).json({
-//     status: "success",
-//     message: "EmmCore Global Networks API is running",
-//   });
-// });
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoute);
